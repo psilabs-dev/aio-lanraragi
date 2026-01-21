bf2dfe721c9d499ef3a7618cca1a88a162a5de41
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2026-01-21 12:05:10 -0800

    move search test case out of test_with_archives

--- integration_tests/tests/simple/test_with_archives.py
+++ integration_tests/tests/simple/test_with_archives.py
@@ -40,10 +40,6 @@ from lanraragi.models.misc import (
     GetOpdsCatalogRequest,
     RegenerateThumbnailRequest
 )
-from lanraragi.models.search import (
-    GetRandomArchivesRequest,
-    SearchArchiveIndexRequest
-)
 from lanraragi.models.tankoubon import (
     AddArchiveToTankoubonRequest,
     CreateTankoubonRequest,
@@ -410,77 +406,6 @@ async def test_archive_category_interaction(lrr_client: LRRClient, semaphore: as
     assert bookmark_cat_id not in cat_ids_cleared, "Archive still reported in bookmark category after removal"
     del resp_cleared, err_cleared
     # <<<<< VERIFY ARCHIVE CATEGORY MEMBERSHIP CLEARED VIA ARCHIVE API <<<<<
-
-    # no error logs
-    expect_no_error_logs(environment, LOGGER)
-
-@pytest.mark.flaky(reruns=2, condition=sys.platform == "win32", only_rerun=r"^ClientConnectorError")
-@pytest.mark.asyncio
-async def test_search_api(lrr_client: LRRClient, semaphore: asyncio.Semaphore, npgenerator: np.random.Generator, environment: AbstractLRRDeploymentContext):
-    """
-    Very basic functional test of the search API.
-    
-    1. upload 100 archives
-    2. search for 20 archives using the search API
-    3. search for 20 archives using random search API
-    4. search for 20 archives using random search API with newonly=true
-    5. search for 20 archives using random search API with untaggedonly=true (should return empty)
-    """
-    num_archives = 100
-
-    # >>>>> TEST CONNECTION STAGE >>>>>
-    response, error = await lrr_client.misc_api.get_server_info()
-    assert not error, f"Failed to connect to the LANraragi server (status {error.status}): {error.error}"
-
-    LOGGER.debug("Established connection with test LRR server.")
-    # verify we are working with a new server.
-    response, error = await lrr_client.archive_api.get_all_archives()
-    assert not error, f"Failed to get all archives (status {error.status}): {error.error}"
-    assert len(response.data) == 0, "Server contains archives!"
-    del response, error
-    assert not any(environment.archives_dir.iterdir()), "Archive directory is not empty!"
-    # <<<<< TEST CONNECTION STAGE <<<<<
-
-    # >>>>> UPLOAD STAGE >>>>>
-    with tempfile.TemporaryDirectory() as tmpdir:
-        tmpdir = Path(tmpdir)
-        LOGGER.debug(f"Creating {num_archives} archives to upload.")
-        write_responses = save_archives(num_archives, tmpdir, npgenerator)
-        assert len(write_responses) == num_archives, f"Number of archives written does not equal {num_archives}!"
-
-        # archive metadata
-        LOGGER.debug("Uploading archives to server.")
-        await upload_archives(write_responses, npgenerator, semaphore, lrr_client, force_sync=ENABLE_SYNC_FALLBACK)
-    # <<<<< UPLOAD STAGE <<<<<
-
-    # >>>>> SEARCH STAGE >>>>>
-    # TODO: current test design limits ability to test results of search (e.g. tag filtering), will need to unravel logic for better test transparency
-    response, error = await lrr_client.search_api.search_archive_index(SearchArchiveIndexRequest())
-    assert not error, f"Failed to search archive index (status {error.status}): {error.error}"
-    assert len(response.data) == 100
-    del response, error
-
-    response, error = await lrr_client.search_api.get_random_archives(GetRandomArchivesRequest(count=20))
-    assert not error, f"Failed to get random archives (status {error.status}): {error.error}"
-    assert len(response.data) == 20
-    del response, error
-
-    response, error = await lrr_client.search_api.get_random_archives(GetRandomArchivesRequest(count=20, newonly=True))
-    assert not error, f"Failed to get random archives (status {error.status}): {error.error}"
-    assert len(response.data) == 20
-    del response, error
-
-    response, error = await lrr_client.search_api.get_random_archives(GetRandomArchivesRequest(count=20, untaggedonly=True))
-    assert not error, f"Failed to get random archives (status {error.status}): {error.error}"
-    assert len(response.data) == 0
-    del response, error
-    # <<<<< SEARCH STAGE <<<<<
-
-    # >>>>> DISCARD SEARCH CACHE STAGE >>>>>
-    response, error = await lrr_client.search_api.discard_search_cache()
-    assert not error, f"Failed to discard search cache (status {error.status}): {error.error}"
-    del response, error
-    # <<<<< DISCARD SEARCH CACHE STAGE <<<<<
 
     # no error logs
     expect_no_error_logs(environment, LOGGER)
