99a819c792e519462f4c5865f7ab1905738a3c56
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2026-03-01 17:00:48 -0800

    remove dead code

--- src/lanraragi/clients/res_processors/misc.py
+++ src/lanraragi/clients/res_processors/misc.py
@@ -4,7 +4,6 @@ from lanraragi.models.misc import (
     GetAvailablePluginsResponse,
     GetAvailablePluginsResponsePlugin,
     GetServerInfoResponse,
-    UsePluginResponse,
 )
 
 _available_plugins_adapter = TypeAdapter(list[GetAvailablePluginsResponsePlugin])
@@ -15,11 +14,7 @@ def _process_get_server_info_response(content: str) -> GetServerInfoResponse:
 def _handle_get_available_plugins_response(content: str) -> GetAvailablePluginsResponse:
     return GetAvailablePluginsResponse(plugins=_available_plugins_adapter.validate_json(content))
 
-def _handle_use_plugin_response(content: str) -> UsePluginResponse:
-    return UsePluginResponse.model_validate_json(content)
-
 __all__ = [
     "_process_get_server_info_response",
     "_handle_get_available_plugins_response",
-    "_handle_use_plugin_response"
 ]
