9d10f67d567845c59a4cc1f88b3f6b5a5f5bc5e5
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2026-03-16 22:34:21 -0700

    add shared_buffers and work_mem

--- integration_tests/src/aio_lanraragi_tests/deployment/factory.py
+++ integration_tests/src/aio_lanraragi_tests/deployment/factory.py
@@ -82,6 +82,8 @@ def generate_deployment(
             use_postgres: bool = request.config.getoption("--postgres")
             if use_postgres:
                 postgres_jit: bool = request.config.getoption("postgres_jit")
+                postgres_shared_buffers_mb: int = request.config.getoption("--postgres-shared-buffers")
+                postgres_work_mem_mb: int = request.config.getoption("--postgres-work-mem")
                 environment = DockerPostgresLRRDeploymentContext(
                     build_path, image, git_url, git_ref, docker_client, staging_dir, resource_prefix, port_offset,
                     build_ref=build_ref, dockerfile=dockerfile, docker_api=docker_api,
@@ -90,6 +92,8 @@ def generate_deployment(
                     cache_backend=ContainerLRRCacheBackend(cache_backend),
                     container_runtime=container_runtime,
                     postgres_jit=postgres_jit,
+                    postgres_shared_buffers_mb=postgres_shared_buffers_mb,
+                    postgres_work_mem_mb=postgres_work_mem_mb,
                 )
             else:
                 environment = ContainerLRRDeploymentContext(
